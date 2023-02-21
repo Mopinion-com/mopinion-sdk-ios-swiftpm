b8b694f1a6874d7f0e80b1659477a628ccb0a7f1
Kees van Welsenis <kvwelsenis@mopinion.com>
2023-02-21 23:05:37 +0100

    draft release: add Package.swift for 1.0.0-beta-19.

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let githubReleaseDir = "1.0.0-beta-19"
+let sdkVersion = "1.0.0-beta-19"
+let sdkZipfileChecksum = "23ad374750f4d9c08411529efa0b991403b766aa89eeb843ba1f4693b0c828ec"
+
+let package = Package(
+    name: "MopinionSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MopinionSDK",
+            targets: ["MopinionSDK"]),
+    ],
+    targets: [
+       .binaryTarget(
+       		name: "MopinionSDK",
+       		url: "https://github.com/mopinion-com/mopinion-sdk-ios/releases/download/\(githubReleaseDir)/MopinionSDK-\(sdkVersion).xcframework.zip",
+       		checksum: sdkZipfileChecksum )     
+    ]
+)
+
