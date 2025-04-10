dd16c9830fa6ef92b0da5e0f00e4c8f9305c1938
Kees van Welsenis <kvwelsenis@mopinion.com>
2025-04-10 17:59:16 +0200

    Version 1.2.1

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@
 
 import PackageDescription
 
-let githubReleaseDir = "1.2.0"
-let sdkVersion = "1.2.0"
-let sdkZipfileChecksum = "f7f98655d8e6741abed502960cb2a51f14c559459750627ed754c4b717f5ffb6"
+let githubReleaseDir = "1.2.1"
+let sdkVersion = "1.2.1"
+let sdkZipfileChecksum = "85b1e3d02499b810f19c4b1a9d42d60357431661b4537985faa9e136e263f6b4"
 
 let package = Package(
     name: "MopinionSDK",
